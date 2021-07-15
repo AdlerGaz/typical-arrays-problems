e3fcb4b2551b018b6aa53344e0d043be347f3b82
Yury Maniakhin <manah9191@mail.ru>
2021-07-15 19:09:16 +0300

    feat: write functions min max average

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array == undefined || array.length == 0) return 0;
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+        min = min > array[i] ? array[i] : min;
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array == undefined || array.length == 0) return 0;
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        max = max < array[i] ? array[i] : max;
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == undefined || array.length == 0) return 0;
+    let sum = array[0];
+    for (let i = 1; i < array.length; i++) {
+        sum += array[i];
+    }
+    return +((sum / array.length).toFixed(2));
 }
